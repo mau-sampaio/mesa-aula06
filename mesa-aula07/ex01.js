7d8ff9f3383814b2c77df9dc71b0de2b86cf4951
mau-sampaio <mauricio-sampaio97@hotmail.com>
2022-08-15 22:04:03 -0300

    ajustado titulo das atividades

--- mesa-aula07/ex01.js
+++ mesa-aula07/ex01.js
@@ -1,31 +1,33 @@
-//Atividade 01
+
+console.log("Atividade 01");
 console.log(!true);
 
-//Atividade 02
+
+console.log("Atividade 02");
 console.log(!false);
 
-//Atividade 03
+console.log("Atividade 03");
 console.log(!!false && true);
 
-//Atividade 04
+console.log("Atividade 04");
 console.log(!!true);
 
-//Atividade 05
+console.log("Atividade 05");
 console.log(!1);
 
-//Atividade 06
+console.log("Atividade 06");
 console.log(!!1);
 
-//Atividade 07
+console.log("Atividade 07");
 console.log(!0);
 
-//Atividade 08
+console.log("Atividade 08");
 console.log(!!0);
 
-//Atividade 09
+console.log("Atividade 09");
 console.log(!!"");
 
-//Atividade 10
+console.log("Atividade 10");
 let x=5;
 let y=9;
     console.log(x<10 && x!==5);
