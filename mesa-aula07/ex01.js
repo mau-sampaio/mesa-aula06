f7cb982ee8b396cbc95fc163c1a4305bc5c1baa6
mau-sampaio <mauricio-sampaio97@hotmail.com>
2022-08-15 21:38:09 -0300

    atividade da mesa aula 05

---
+++ mesa-aula07/ex01.js
@@ -0,0 +1,33 @@
+//Atividade 01
+console.log(!true);
+
+//Atividade 02
+console.log(!false);
+
+//Atividade 03
+console.log(!!false && true);
+
+//Atividade 04
+console.log(!!true);
+
+//Atividade 05
+console.log(!1);
+
+//Atividade 06
+console.log(!!1);
+
+//Atividade 07
+console.log(!0);
+
+//Atividade 08
+console.log(!!0);
+
+//Atividade 09
+console.log(!!"");
+
+//Atividade 10
+let x=5;
+let y=9;
+    console.log(x<10 && x!==5);
+    console.log(x>9 || x===5);
+    console.log(!(x===y));
---
+++ mesa-aula07/ex02.js
@@ -0,0 +1,15 @@
+//Atividade 01
+let x=10;
+let y="a";
+console.log(y==="b" || x>=10);
+
+//Atividade 02
+x=3;
+y=8;
+console.log(!(x=="3" || x===y) && !(y!==8&&x<=y));
+
+//Atividade 03
+let str ="";
+let msg = "haha";
+let eBonito ="false";
+console.log(!((str || msg) && eBonito));
